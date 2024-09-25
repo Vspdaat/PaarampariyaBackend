04ae9dab22b56e2fdad6cdbd56fa5323091296de
Mohan <mohanpraveen4161@gmail.com>
2024-09-25 20:20:52 +0530

    production

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,20 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
- 
-// deployment
 
+// deployment
+__dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    });
+} else {
     app.get('/', (req, res) => {
         res.send('Server is Running! 🚀');
     });
+}
 
 const server = app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
